f235fc1bd5b6206f31852b0fea6a4766d595a757
Roman Perehrest <perehrestich@gmail.com>
2018-10-11 19:37:54 +0300

    ini

---
+++ GetLocation/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace GetLocation
+{
+    [Activity(Label = "Lab 6", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
